7fe363b9a6706ae37d97a63b2fdbf0a16fccebd9
Evgeniy <kievtechnoshop@gmail.com>
2020-05-01 22:38:56 +0300

    Updates

---
+++ precache-manifest.d82c1789afcc9d3f11c6e46d8c986fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40c601d581e9b8be838b7987b428834",
+    "url": "/react_geojson_map/index.html"
+  },
+  {
+    "revision": "2746f8d7555fe20956ea",
+    "url": "/react_geojson_map/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "12fa4fb2cc7129ae8000",
+    "url": "/react_geojson_map/static/css/main.d98b5536.chunk.css"
+  },
+  {
+    "revision": "2746f8d7555fe20956ea",
+    "url": "/react_geojson_map/static/js/2.396896bd.chunk.js"
+  },
+  {
+    "revision": "12fa4fb2cc7129ae8000",
+    "url": "/react_geojson_map/static/js/main.4a51540a.chunk.js"
+  },
+  {
+    "revision": "798ce06ca8f7c562d7dc",
+    "url": "/react_geojson_map/static/js/runtime-main.65b87164.js"
+  }
+]);
